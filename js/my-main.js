7a83874af1e421afc36c26ceb75a49a7dd943cdf
mahmoud-ahmed-2002 <mahmoud.biltaji.2002@gmail.com>
2023-04-29 08:05:02 +0300

    add daschboard

--- js/my-main.js
+++ js/my-main.js
@@ -3,3 +3,20 @@ $("#addComment").click(function () {
     $("#addComment").hide();
     $("#form-group-comment").slideDown();
   });
+
+
+  // ----------------- edit layout of page dacboard 
+  
+  window.onresize = () => { 
+    refreWidth()
+  }
+  
+  
+  function refreWidth(){
+    let dasboardWidth =$('.dasboard-meun').outerWidth() + 5 +'px';
+    let pageTopbarHeight = $('.page-topbar').outerHeight() +5 +'px';
+    $('.page-topbar').css('left',dasboardWidth);
+    $('.main-content').css({'left':dasboardWidth ,'top':pageTopbarHeight});
+
+  }
+  refreWidth()
