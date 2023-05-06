f540a11ead206bd6d90d2bbe8058b51e54051e35
mahmoud-ahmed-2002 <mahmoud.biltaji.2002@gmail.com>
2023-05-06 23:42:48 +0300

    edit the english page

--- englich/js/my-main.js
+++ englich/js/my-main.js
@@ -1,5 +1,30 @@
 // -------------- slid down and up section add queation
 $("#addComment").click(function () {
-    $("#addComment").hide();
-    $("#form-group-comment").slideDown();
-  });
+  $("#addComment").hide();
+  $("#form-group-comment").slideDown();
+});
+
+
+//  ---------------------- snipeer
+
+$(document).ready(function () {
+  $(".snipeer").fadeOut(3000);
+});
+
+//  ---------------------- navbar scroll script 
+
+
+let headerNav = document.querySelector('.header');
+let navScroll = document.querySelector('.nav-scroll');
+
+console.log(navScroll)
+
+onscroll = (event) => {
+  if (window.pageYOffset > headerNav.clientHeight) {
+    // headerNav.classList.add('d-none');
+    navScroll.classList.add('scroll');
+  } else {
+    // headerNav.classList.remove('d-none');
+    navScroll.classList.remove('scroll');
+  }
+};
